d10a7461c24da7d06ea7708a82c22be6cb1f4b8a
Jason Linehan <patientulysses@gmail.com>
2012-04-22 13:50:18 -0400

    Fixed the pullup script

--- pullup.sh
+++ pullup.sh
@@ -3,8 +3,15 @@
 # Pulls (git pull) the latest commit of the plugin repositories in 
 # the ./repo directory.
 
+cloneum()
+{
+        git clone https://github.com/scrooloose/nerdcommenter.git
+        git clone https://github.com/scrooloose/nerdtree.git
+        git clone https://github.com/godlygeek/tabular.git
+}
+
+
 for i in `find ./repo -maxdepth 1 -mindepth 1 -type d`; do
         cd $i
-        git pull
-        cd - 
+        [ "$(ls -A 2>/dev/null)" ] && git pull && cd - || cd - || cloneum
 done
